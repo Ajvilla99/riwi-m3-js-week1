1cca221421682eada1fe2bb9d847fd3490ddd16f
abrahan villa <ajvilla1999@gmail.com>
2025-06-19 21:53:04 -0500

    ft: changed the else if validation because the main variable was used without parsing

--- sistema_interactivo.js
+++ sistema_interactivo.js
@@ -3,37 +3,43 @@ let idCounter = 0
 const tbody = document.getElementById('table_body')
 
 const add_user = () => {
-    // Capturamos y pedimos los datos al usuario
+    console.clear()
+    // we capture and request user data.
     let user_name = prompt('Digite su nombre')
     let user_age = prompt('Digite su edad')
-    // Convertimos la edad en entero
+    
+    // We convert the age to an integer
     age = parseInt(user_age)
-
-    // Validaciones y muestra el mensaje
+    console.log(age);
+    // Validations and displays the message
     if (isNaN(age)) {
         console.error('Error: Por favor, ingresa una edad válida en números.')
-    } else if (user_age < 18) {
+        alert('Error: Por favor, ingresa una edad válida en números.')
+        return
+    } else if (age < 18) {
         alert(`Hola ${user_name}, eres menor de edad. ¡Sigue aprendiendo y disfrutando del código!`)
     } else {
         alert(`Hola ${user_name}, eres mayor de edad. ¡Prepárate para grandes oportunidades en el mundo de la programación!.`)
     }
 
-    users.push({ id: idCounter ,'name': user_name, 'age': user_age, 'state': true })
+    // temporary saving
+    users.push({ 'id': idCounter ,'name': user_name, 'age': age, 'state': true })
 
     // create row
     const row = document.createElement('div');
     row.classList.add('table_row');
 
-    // Crear cells
+    // create cells
     row.innerHTML = `
       <div class="table_row_item">${idCounter++}</div>
       <div class="table_row_item">${user_name}</div>
-      <div class="table_row_item">${user_age}</div>
+      <div class="table_row_item">${age}</div>
       <div class="table_row_item">${'active'}</div>
     `;
 
-    // Agregar al cuerpo de la tabla
+    // Add to the body of the table
     tbody.appendChild(row);
 
+    // I print on the console to see the added user
     console.log(users);
 }
